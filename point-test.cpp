f06c1bd7c23d6284c132354e664dfe87038d4127
Aiswarya PS <69056143+AiswaryaPS@users.noreply.github.com>
2021-02-06 16:27:46 +0530

    Update point-test.cpp

--- point-test.cpp
+++ point-test.cpp
@@ -6,17 +6,17 @@ TEST(Point,Empty_Constructor) {
     Point P;
     EXPECT_EQ(0.0,P.distanceFromOrigin());
     EXPECT_EQ(true,P.isOrigin());
-    EXPECT_EQ(false,P.isOnXAxis());
-    EXPECT_EQ(false,P.isOnYAxis());
+    EXPECT_EQ(false,P.isOnXaxis());
+    EXPECT_EQ(false,P.isOnYaxis());
 }
 
 TEST(Point,Parameterized_Cosnstructor) {
     Point P(5,12);
     EXPECT_EQ(13.0,P.distanceFromOrigin());
     EXPECT_EQ(1,P.quadrant());
-    EXPECT_EQ(false,P.isOrigin());
-    EXPECT_EQ(false,P.isOnXAxis());
-    EXPECT_EQ(false,P.isOnYAxis());
+    EXPECT_EQ(false,P.isorigin());
+    EXPECT_EQ(false,P.isonXaxis());
+    EXPECT_EQ(false,P.isonYaxis());
 
 }
 
@@ -25,9 +25,9 @@ TEST(Point,Copy_Cosnstructor) {
     Point P2(P3);
     EXPECT_EQ(13,P4.distanceFromOrigin());
     EXPECT_EQ(3,P4.quadrant());
-    EXPECT_EQ(false,P4.isOrigin());
-    EXPECT_EQ(false,P4.isOnXAxis());
-    EXPECT_EQ(false,P4.isOnYAxis());
+    EXPECT_EQ(false,P4.isorigin());
+    EXPECT_EQ(false,P4.isonXaxis());
+    EXPECT_EQ(false,P4.isonYaxis());
 
 }
 }
